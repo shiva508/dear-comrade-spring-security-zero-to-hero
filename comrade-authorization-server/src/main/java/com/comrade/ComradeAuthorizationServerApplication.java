0815ffc9a8dae43ee78efbecfd1dcac1ff2d5e9b
shiva508 <dasarishiva1@gmail.com>
2025-07-19 17:55:59 +0530

    1. Generate token

---
+++ comrade-authorization-server/src/main/java/com/comrade/ComradeAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComradeAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComradeAuthorizationServerApplication.class, args);
+    }
+
+}
